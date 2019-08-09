9c8477e44cf2ecd97bd48e12119d95248bc968a0
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-08-09 11:39:33 -0700

    added code to game.js

---
+++ game.js
@@ -0,0 +1,30 @@
+
+
+
+
+var wordBank = ["Charizard", "Gengar", "Alakazam", "Gyrados", "Kabutops", "Victreebel", "Machamp", "Nidoking", "Snorlax", "Golem"],
+
+
+var pickWord = function() {
+    var wordNumber = Math.floor(Math.random() * 10);
+    console.log(wordNumber)
+    return wordNumber;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

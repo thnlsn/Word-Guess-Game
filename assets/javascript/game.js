ae29d35fac30f50861e92b0b73c3687f599b3d37
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-08-09 16:07:15 -0700

    added images, css, and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+
+//STATUS
+var wins = 0;
+var losses = 0;
+var remainingGuesses = 0;
+var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+var failedGuesses = [];
+
+//WORD BANK
+var wordBank = [
+    "Charizard",
+    "Gengar",
+    "Alakazam",
+    "Gyrados",
+    "Kabutops",
+    "Victreebel",
+    "Machamp",
+    "Nidoking",
+    "Snorlax",
+    "Golem"
+];
+
+//PICK WORD FUNCTION
+var pickWord = function() {
+   
+   var wordNumber = Math.floor(Math.random() * 10);
+   return wordNumber;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(wordBank);
